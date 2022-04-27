21ce8c7e3e7998c23ef6b1fae59ee9f4ea9c8cdb
Justin Meimar <j.meimar111@gmail.com>
2022-04-27 12:47:25 -0600

    parsing

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,22 @@
 #![allow(unused)]
 use clap::Parser;
 use std::fs::File;
-
+use std::fs;
 use std::io;
-use std::io::Read;
+use std::io::{Read, BufReader, BufRead, Error};
 
+// structs
 #[derive(Parser)]
 pub struct Cli {
     pattern: String,
     #[clap(parse(from_os_str))] 
     path: std::path::PathBuf,
 
+}
+
+pub struct Program {
+    pub file_name: &'static str,
+    pub memory: [u8; 4096], // MAX program size is 4096 lines
 }
 
 pub struct CPU {
@@ -31,6 +37,23 @@ pub fn read_word(pc: u16, memory: [u8; 4096]) -> u16 {
     ( (memory[idx as usize] as u16) << 8 | ( memory[idx as usize + 1] ) as u16 ) as u16
 }
 
+// implement traits on structs
+impl Program {
+
+    //pub fn load_program(path: std::path::PathBuf) -> Result<Vec<u8>, Box<dyn std::error::Error>>{
+    pub fn load_program(&self, path: std::path::PathBuf) {   
+        
+        let mut data = fs::read_to_string(path);
+        for byte in &data {
+            self.parse_instr( byte.to_string() ) 
+        }          
+    }
+    pub fn parse_instr(&mut self, string : std::string::String){
+        println!("{}", string)
+    }
+
+}
+
 impl CPU {
     pub fn new_cpu() -> CPU {
         let new_cpu = CPU{
@@ -43,12 +66,23 @@ impl CPU {
     }
     
     pub fn execute_cycle(&mut self) {
-
+        
+        /*
+        //explicit lb $3, $4, $0
         self.mem[self.pc as usize] = 0x03;
         self.mem[self.pc as usize + 1] = 0x40;
+        */
+        
+        //explicit addi $1, 0x127
+        self.mem[self.pc as usize] = 0x71;
+        self.mem[self.pc as usize + 1] = 0xFF;
+
         let word: u16 = read_word(self.pc, self.mem);
         
-        println!("{}", word);
+        println!("instr: {}", word);
+
+        //explicitly set the value at memory to load from to 125
+        self.mem[0x02AA as usize] = 125; 
         self.process_opcode(self.pc, word)
     }
 
@@ -64,12 +98,24 @@ impl CPU {
                 //impl lb
                 println!("load byte");
                 println!("b1: {} b2: {} b3: {}", b1, b2, b3);
-                println!("{}", self.mem[b2 as usize]);
-                self.reg[b2 as usize] = 127;
-                let u = self.reg[b2 as usize];
-                let fetch = self.mem[u as usize + b3 as usize];
-                self.reg[b1 as usize] = fetch;
-                println!("fetch: {}", fetch);    
+                
+                // read 16 bit address from 2 8-bit registers 
+                self.reg[b2 as usize] = 0x02;
+                self.reg[b3 as usize] = 0xAA;
+                
+                
+                let r_up = self.reg[b2 as usize];
+                let r_dwn = self.reg[b3 as usize];
+                
+                // address = r_up << 8 | r_dwn
+                let addr = ((r_up as u16) << 8 | r_dwn as u16) as usize;                
+             
+                println!("mem[addr]: {}", self.mem[addr]);
+                println!("fetch: {}", self.reg[b1 as usize]);
+                               
+                self.reg[b1 as usize] = self.mem[addr];
+
+                println!("fetch: {}", self.reg[b1 as usize]);   
             },
             0x1 => {
                 //impl sb
@@ -82,10 +128,20 @@ impl CPU {
             0x2 => println!("and"),
             0x3 => println!("or"),
             0x4 => println!("xor"),
+
+            
+            0x5 => println!("nor"),
+            0x6 => println!("add"),
+            0x7 => {
+                // impl addi  op $r1 |-- value --| range(0--127)
+                println!("addi");
+                println!("reg before: {}", self.reg[b1 as usize]);
+                let imm = ((b2 as u8) << 4) | b2 as u8;
+                self.reg[b1 as usize] = imm;
+                println!("reg after: {}", self.reg[b1 as usize]);
+            },
             /*
-            0x5 => println!("2"),
-            0x6 => println!("1"),
-            0x7 => println!("2"),  
+              
             0x8 => println!("1"),
             0x9 => println!("2"),
             0xA => println!("1"),
@@ -99,16 +155,16 @@ impl CPU {
 
 fn main() {
 
-    let args = Cli::parse(); 
+   
     let addr = 0x20;
     let mut cpu = CPU::new_cpu();
         cpu.mem[127 as usize] = 0xAB; //171
         cpu.pc = addr;
-        cpu.execute_cycle();
-        
-    println!("pc: {} mem[pc]: {}", cpu.pc, cpu.mem[cpu.pc as usize]);
-    println!("{}",args.pattern); 
-
+    
+    let args = Cli::parse(); 
+    let mut program = Program::load_program( args.path );
+    
+        //cpu.execute_cycle();
 }
 
 
